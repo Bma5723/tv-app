5a27d44308db128dec6eb868836bdc21bf5210ba
Bader Alkatheeri <baderalkatheeri@Baders-MacBook-Pro.local>
2023-12-04 22:01:04 -0500

    second commit

--- src/tv-app.js
+++ src/tv-app.js
@@ -1,81 +1,254 @@
 // import stuff
-import { LitElement, html, css } from 'lit';
-import '@shoelace-style/shoelace/dist/components/dialog/dialog.js';
-import '@shoelace-style/shoelace/dist/components/button/button.js';
+import { LitElement, html, css } from "lit";
 import "./tv-channel.js";
+import "./top-bar.js";
 
 export class TvApp extends LitElement {
-  // defaults
   constructor() {
     super();
-    this.name = '';
-    this.source = new URL('../assets/channels.json', import.meta.url).href;
-    this.listings = [];
+    this.source = new URL("../assets/channels.json", import.meta.url).href;
+    this.listings = []; 
+    this.id = ""; 
+    this.selectedCourse = null; 
+    this.activeIndex = null; 
+    this.activeContent = ""; 
+    this.itemClick = this.itemClick.bind(this); 
+    this.time = ""; 
   }
-  // convention I enjoy using to define the tag's name
+
+  // LitElement life cycle for when the element is added to the DOM
+  connectedCallback() {
+    super.connectedCallback(); // helps in setting up the initial state of the component
+  }
+
   static get tag() {
-    return 'tv-app';
+    return "tv-app";
   }
+
   // LitElement convention so we update render() when values change
   static get properties() {
     return {
       name: { type: String },
-      source: { type: String },
+      source: { type: String }, // To fetch the JSON file
       listings: { type: Array },
+      selectedCourse: { type: Object },
+      contents: { type: Array },
+      id: { type: String },
+      activeIndex: { type: Number },
+      activeContent: { type: String },
+      //time: { type: String },
     };
   }
   // LitElement convention for applying styles JUST to our element
   static get styles() {
     return [
       css`
-      :host {
-        display: block;
-        margin: 16px;
-        padding: 16px;
-      }
-      `
+        :host {
+          display: block;
+          margin: 16px;
+          padding: 16px;
+        }
+
+        .alignContent {
+          display: flex;
+          justify-content: flex-start;
+          gap: 90px;
+        }
+
+        .course-topics {
+          margin-left: -36px;
+          display: flex;
+          flex-direction: column;
+          width: 275px;
+          margin-right: 1px;
+          margin-top: 25px;
+          position: fixed;
+          padding-top: 8px;
+          padding-right: 5px;
+        }
+
+        .main {
+          margin: 42px 141px 23px 386px;
+          padding-top: 8px;
+          padding-right: 5px;
+          padding-bottom: 1px;
+          padding-left: 20px;
+          width: calc(100% - 291px);
+          height: 100%;
+          font-size: 1em;
+          border: 1px solid #dadce0;
+          border-radius: 5px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+          background-color: #f8f9fa;
+          font: 400 16px/24px var(--devsite-primary-font-family);
+          -webkit-font-smoothing: antialiased;
+          text-size-adjust: 100%;
+          color: #4e5256;
+          font-family: var(--devsite-primary-font-family);
+          background: #f8f9fa;
+        }
+
+        .fabs {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          position: fixed;
+          bottom: 0;
+          right: 0;
+          margin: 19px;
+          width: 81vw;
+        }
+
+        #previous > button {
+          border-radius: 4px;
+          font-family:
+            Google Sans,
+            Arial,
+            sans-serif;
+          font-size: 14px;
+          font-weight: 600;
+          letter-spacing: 0.6px;
+          line-height: 24px;
+          padding-bottom: 6px;
+          padding-left: 24px;
+          padding-right: 24px;
+          padding-top: 6px;
+          pointer-events: auto;
+          text-transform: none;
+          background: #fff;
+          color: #1a73e8;
+          border: 0;
+          box-shadow:
+            0 2px 2px 0 rgba(0, 0, 0, 0.14),
+            0 1px 5px 0 rgba(0, 0, 0, 0.12),
+            0 3px 1px -2px rgba(0, 0, 0, 0.2);
+        }
+        #next > button {
+          border-radius: 4px;
+          font-family:
+            Google Sans,
+            Arial,
+            sans-serif;
+          font-size: 14px;
+          font-weight: 600;
+          letter-spacing: 0.6px;
+          line-height: 24px;
+          padding-bottom: 6px;
+          padding-left: 24px;
+          padding-right: 24px;
+          padding-top: 6px;
+          pointer-events: auto;
+          text-transform: none;
+          background: #1a73e8;
+          color: #fff;
+          border: 0;
+          box-shadow:
+            0 2px 2px 0 rgba(0, 0, 0, 0.14),
+            0 1px 5px 0 rgba(0, 0, 0, 0.12),
+            0 3px 1px -2px rgba(0, 0, 0, 0.2);
+        }
+      `,
     ];
   }
-  // LitElement rendering template of your element
+
   render() {
     return html`
-      <h2>${this.name}</h2>
-      ${
-        this.listings.map(
-          (item) => html`
-            <tv-channel 
-              title="${item.title}"
-              presenter="${item.metadata.author}"
-              @click="${this.itemClick}"
-            >
-            </tv-channel>
-          `
-        )
-      }
-      <div>
-        <!-- video -->
-        <!-- discord / chat - optional -->
+      <top-bar time="${this.time}"> </top-bar>
+      <div class="alignContent">
+        <div class="course-topics">
+          ${this.listings.map(
+            (item, index) => html`
+              <tv-channel
+                title="${item.title}"
+                id="${item.id}"
+                @click="${() => this.itemClick(index)}"
+                activeIndex="${this.activeIndex}"
+              >
+              </tv-channel>
+            `,
+          )}
+        </div>
+
+        <div class="main">
+          <slot> ${this.activeContent} </slot>
+        </div>
+
+        <div class="fabs">
+          <div id="previous">
+            <button @click=${() => this.prevPage()}>Back</button>
+          </div>
+          <div id="next">
+            <button @click=${() => this.nextPage()}>Next</button>
+          </div>
+        </div>
       </div>
-      <!-- dialog -->
-      <sl-dialog label="Dialog" class="dialog">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        <sl-button slot="footer" variant="primary" @click="${this.closeDialog}">Close</sl-button>
-      </sl-dialog>
     `;
   }
 
-  closeDialog(e) {
-    const dialog = this.shadowRoot.querySelector('.dialog');
-    dialog.hide();
+  async nextPage() {
+    if (this.activeIndex !== null) {
+      const nextIndex = this.activeIndex + 1;
+      const item = this.listings[nextIndex].location;
+
+      const contentPath = "/assets/" + item;
+
+      try {
+        const response = await fetch(contentPath);
+        this.activeContent = await response.text();
+        this.activeIndex = nextIndex; 
+      } catch (err) {
+        console.log("fetch failed", err);
+      }
+    }
   }
 
-  itemClick(e) {
-    console.log(e.target);
-    const dialog = this.shadowRoot.querySelector('.dialog');
-    dialog.show();
+  // function to fetch the previous content
+  async prevPage() {
+    if (this.activeIndex !== null) {
+
+      const prevIndex = this.activeIndex - 1; 
+
+      const item = this.listings[prevIndex].location; 
+
+      const contentPath = "/assets/" + item;
+
+      try {
+        const response = await fetch(contentPath);
+        this.activeContent = await response.text();
+        //console.log("Active Content", this.activeContent);
+        this.activeIndex = prevIndex; // Update the active index after fetching content
+      } catch (err) {
+        console.log("fetch failed", err);
+      }
+    }
   }
 
-  // LitElement life cycle for when any property changes
+  // Funtion to fetch for the content that is being clicked
+  async itemClick(index) {
+    this.activeIndex = index; 
+    //console.log("Active Index: ", this.activeIndex);
+
+    const item = this.listings[index].location; 
+    //console.log("Active Content: ", item);
+
+    this.time = this.listings[index].metadata.timecode; 
+    //console.log("Time: ", this.time);
+
+    const contentPath = "/assets/" + item;
+
+    
+    try {
+      const response = await fetch(contentPath);
+      // console.log("Response: ", response);
+      const text = await response.text();
+      // console.log("Text: ", text);
+      this.activeContent = text; 
+    } catch (err) {
+      console.log("fetch failed", err);
+    }
+  }
+
+
   updated(changedProperties) {
     if (super.updated) {
       super.updated(changedProperties);
@@ -87,13 +260,22 @@ export class TvApp extends LitElement {
     });
   }
 
+ 
   async updateSourceData(source) {
-    await fetch(source).then((resp) => resp.ok ? resp.json() : []).then((responseData) => {
-      if (responseData.status === 200 && responseData.data.items && responseData.data.items.length > 0) {
-        this.listings = [...responseData.data.items];
-      }
-    });
+    await fetch(source)
+      .then((resp) => (resp.ok ? resp.json() : []))
+      .then((responseData) => {
+        if (
+          responseData.status === 200 &&
+          responseData.data.items &&
+          responseData.data.items.length > 0
+        ) {
+          this.listings = [...responseData.data.items]; 
+          console.log("Listings: ", this.listings);
+        }
+      });
   }
 }
+
 // tell the browser about our tag and class it should run when it sees it
 customElements.define(TvApp.tag, TvApp);
